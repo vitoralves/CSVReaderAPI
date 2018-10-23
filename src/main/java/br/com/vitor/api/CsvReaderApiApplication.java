f9921f1ab0cf881867f1a96777ed5a490649128e
Vitor DIogo Alves <vitor.diogo.live@hotmail.com>
2018-10-23 19:29:04 -0300

    initial commit

---
+++ src/main/java/br/com/vitor/api/CsvReaderApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.vitor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvReaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvReaderApiApplication.class, args);
+	}
+}
